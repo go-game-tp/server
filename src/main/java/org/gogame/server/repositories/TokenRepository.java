88986329c91e29c7c365fb8e8a7330133049abfc
Roman Szlachtun <lidroma2@gmail.com>
2024-01-27 21:25:13 +0100

    add token table

---
+++ src/main/java/org/gogame/server/repositories/TokenRepository.java
@@ -0,0 +1,11 @@
+package org.gogame.server.repositories;
+
+import org.gogame.server.domain.entities.TokenEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<TokenEntity, Long> {
+}
