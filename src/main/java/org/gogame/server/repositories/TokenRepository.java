f0ebe6ca7f750e0c2c7d94a96b5ea7209a66f187
Roman Szlachtun <lidroma2@gmail.com>
2024-01-27 22:05:52 +0100

    add token repo

--- src/main/java/org/gogame/server/repositories/TokenRepository.java
+++ src/main/java/org/gogame/server/repositories/TokenRepository.java
@@ -8,4 +8,17 @@ import java.util.List;
 import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<TokenEntity, Long> {
+
+    @Query(value = """
+            SELECT t
+            FROM TokenEntity t
+            INNER JOIN UserEntity u
+            ON t.user.userId = u.userId
+            WHERE u.userId = :userId
+            AND t.revoked = false
+            AND t.expired = false
+            """)
+    List<TokenEntity> findAllValidTokensByUser(Long userId);
+
+    Optional<TokenEntity> findByToken(String token);
 }
