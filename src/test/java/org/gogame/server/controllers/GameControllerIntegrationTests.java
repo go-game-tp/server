dff6fe0daa8ae7c5ae199d137cdceede96e3b8d2
jacekpoz <jacekpoz@cock.li>
2024-01-28 17:37:46 +0100

    rework invite can be sent and add can be accepted

--- src/test/java/org/gogame/server/controllers/GameControllerIntegrationTests.java
+++ src/test/java/org/gogame/server/controllers/GameControllerIntegrationTests.java
@@ -1,5 +1,7 @@
 package org.gogame.server.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.gogame.server.domain.entities.dto.UserInviteDto;
 import org.gogame.server.domain.entities.dto.UserLoginDto;
@@ -17,6 +19,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -41,16 +44,70 @@ public class GameControllerIntegrationTests {
 
     @Test
     public void testThatGameInviteCanBeSent() throws Exception {
-        UserInviteDto inviteA = TestData.InviteDtoUtils.createA(userRepo);
-        String inviteJson = objectMapper.writeValueAsString(inviteA);
+
+        UserRegisterDto regA = TestData.RegisterDtoUtils.createA();
+        MvcResult mvcAResult = ControllerUtils.registerUser(mockMvc, objectMapper, regA);
+
+        UserRegisterDto regB = TestData.RegisterDtoUtils.createB();
+        MvcResult mvcBResult = ControllerUtils.registerUser(mockMvc, objectMapper, regB);
+
+
+        UserInviteDto invite = UserInviteDto.builder()
+                .userSenderId(ControllerUtils.getUserId(mvcAResult))
+                .userReceiverId(ControllerUtils.getUserId(mvcBResult))
+                .build();
+
+        sendGameInvite(
+                invite,
+                ControllerUtils.getJwtToken(mvcAResult)
+        );
+    }
+
+    @Test
+    public void testThatGameInviteCanBeAccepted() throws Exception {
+
+        UserRegisterDto regA = TestData.RegisterDtoUtils.createA();
+        MvcResult mvcAResult = ControllerUtils.registerUser(mockMvc, objectMapper, regA);
+
+        UserRegisterDto regB = TestData.RegisterDtoUtils.createB();
+        MvcResult mvcBResult = ControllerUtils.registerUser(mockMvc, objectMapper, regB);
+
+        var senderId = ControllerUtils.getUserId(mvcAResult);
+        var receiverId = ControllerUtils.getUserId(mvcBResult);
+
+        UserInviteDto invite = UserInviteDto.builder()
+                .userSenderId(senderId)
+                .userReceiverId(receiverId)
+                .build();
+
+        sendGameInvite(
+                invite,
+                ControllerUtils.getJwtToken(mvcAResult)
+        );
+
+        String inviteJson = objectMapper.writeValueAsString(invite);
 
         mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/v1/game/invite/accept")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(inviteJson)
+                        .header("Authorization", ControllerUtils.getJwtToken(mvcBResult))
+        ).andExpect(
+                MockMvcResultMatchers.status().is(HttpStatus.OK.value())
+        );
+    }
+
+    private MvcResult sendGameInvite(UserInviteDto userInviteDto, String senderToken) throws Exception {
+        String inviteJson = objectMapper.writeValueAsString(userInviteDto);
+
+        return mockMvc.perform(
                 MockMvcRequestBuilders.post("/api/v1/game/invite/send")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(inviteJson)
+                        .header("Authorization", senderToken)
         ).andExpect(
                 MockMvcResultMatchers.status().is(HttpStatus.CREATED.value())
-        );
+        ).andReturn();
     }
 
 }
