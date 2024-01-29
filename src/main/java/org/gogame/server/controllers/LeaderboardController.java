640fcd81100edbccd963ad09f6c3a4cb718bdaca
jacekpoz <jacekpoz@cock.li>
2024-01-29 06:28:54 +0100

    add leaderboard service and controller

---
+++ src/main/java/org/gogame/server/controllers/LeaderboardController.java
@@ -0,0 +1,51 @@
+package org.gogame.server.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.gogame.server.mappers.Mapper;
+import org.gogame.server.domain.entities.UserBioEntity;
+import org.gogame.server.domain.entities.UserEntity;
+import org.gogame.server.domain.entities.dto.user.UserProfileDto;
+import org.gogame.server.service.LeaderboardService;
+import org.gogame.server.service.PermissionValidatorService;
+import org.gogame.server.service.UserBioService;
+import org.gogame.server.service.UserProfileService;
+import org.springframework.data.util.Pair;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class LeaderboardController {
+
+    private final LeaderboardService leaderboardService;
+    private final PermissionValidatorService validatorService;
+
+    @GetMapping("/leaderboard/{id}")
+    public ResponseEntity<List<UserProfileDto>> getLeaderboard(
+            @PathVariable Long userId,
+            @RequestHeader("Authorization") String token
+    ) {
+
+        if (!validatorService.validateUserId(userId, token)) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        List<UserProfileDto> leaderboard;
+        try {
+            leaderboard = leaderboardService.getLeaderboard(userId);
+        } catch (SQLException ex) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(leaderboard, HttpStatus.OK);
+    }
+}
